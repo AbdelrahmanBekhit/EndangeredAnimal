a39054b377d01df88a1588e6e686e1f708faa29d
AbdelrahmanBekhit <abdelrahman.bekhit@ucalgary.ca>
2025-02-15 12:52:09 -0700

    initialfiles

---
+++ src/main/java/org/src/endangeredanimal/EndangeredAnimalApplication.java
@@ -0,0 +1,13 @@
+package org.src.endangeredanimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndangeredAnimalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndangeredAnimalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/src/endangeredanimal/EndangeredAnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.src.endangeredanimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndangeredAnimalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

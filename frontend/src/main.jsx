ad3790ca076e072c399ba8c6ff6a0ce7105e5e3a
YasnaNa <149362338+YasnaNa@users.noreply.github.com>
2025-02-16 05:47:53 -0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,51 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+// import Landing from './pages/Landing.jsx'
+// import Map from './pages/Map.jsx'
+// import Donate from './pages/Donate.jsx'
+// import Stats from './pages/Stats.jsx'
+// // import Species from './pages/Species.jsx'
+// import Volunteer from './pages/Volunteer.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path : '/',
+//     element: <Landing/>
+//   },
+//   {
+//     path: '/map',
+//     element: <Map/>
+//   },
+//   {
+//     path: '/donate',
+//     element: <Donate/>
+//   },
+//   {
+//     path: '/volunteer',
+//     element: <Volunteer/>
+//   },
+//   {
+//     path: '/stats',
+//     element: <Stats/>
+//   },
+//   // {
+//   //   path: '/species',
+//   //   element: <Species/>
+//   // }
+// ])
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <RouterProvider router={router}/>
+//     {/* <App /> */}
+//   </StrictMode>,
+// )
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -7,7 +55,7 @@ import Landing from './pages/Landing.jsx'
 import Map from './pages/Map.jsx'
 import Donate from './pages/Donate.jsx'
 import Stats from './pages/Stats.jsx'
-// import Species from './pages/Species.jsx'
+import Species from './pages/Species.jsx'
 import Volunteer from './pages/Volunteer.jsx'
 
 const router = createBrowserRouter([
@@ -31,10 +79,10 @@ const router = createBrowserRouter([
     path: '/stats',
     element: <Stats/>
   },
-  // {
-  //   path: '/species',
-  //   element: <Species/>
-  // }
+  {
+    path: '/species',
+    element: <Species/>
+  }
 ])
 
 
